97e1e8f3cde64084a2be37e1b1eb99f81f873d8d
Deividy Metheler <deividyz@gmail.com>
2014-05-01 14:44:36 -0300

    add more specs

--- specs/display.spec.js
+++ specs/display.spec.js
@@ -16,4 +16,86 @@ describe("JA display tests", function () {
 
         $("#homeTest").length.should.be.eql(1);
     });
+
+    // why are we hiting the activate events twice?
+    it('custom methods', function (done) {
+        var steps = 1;
+
+        JA.addPage({ id: "pageTest" }).addDisplay({
+            id: 'pageDisplay',
+            container: 'pageDisplayContainer',
+            beforeDeactivate: function (next) {
+                if (steps > 5) {
+                    steps.should.be.eql(10);
+                } else {
+                    steps.should.be.eql(5);
+                }
+
+                steps++;
+                next();
+            },
+            afterDeactivate: function () { 
+                if (steps > 6) {
+                    steps.should.be.eql(11);
+                } else {
+                    steps.should.be.eql(6);
+                }
+
+                steps++;
+            },
+
+            beforeActivate: function (next) {
+                if (steps > 3) {
+                    steps.should.be.eql(8);
+                } else {
+                    steps.should.be.eql(3);
+                }
+
+                steps++;
+                next();
+            },
+            afterActivate: function () {
+                if (steps > 4) {
+                    steps.should.be.eql(9);
+                } else {
+                    steps.should.be.eql(4);
+                }
+
+                steps++;
+            },
+            render: function () {
+                steps.should.be.eql(1);
+                steps++;
+            }
+        });
+
+        JA.addPage({ id: 'JAPage' }).addDisplay({
+            id: 'jaDisplay',
+            container: 'jaDisplayContainer',
+            beforeActivate: function (next) {
+                if (steps > 7) {
+                    steps.should.be.eql(12);
+                } else {
+                    steps.should.be.eql(7);
+                }
+                steps++;
+                next();
+            },
+            afterActivate: function () {
+                if (steps > 8) {
+                    steps.should.be.eql(13)
+                } else {
+                    steps.should.be.eql(8);
+                }
+                done();
+            },
+            render: function () {
+                steps.should.be.eql(2);
+                steps++;
+            }
+        });
+
+        JA.navigate('pageTest');
+        JA.navigate('JAPage');
+    });
 });
--- specs/page.spec.js
+++ specs/page.spec.js
@@ -26,4 +26,47 @@ describe("JA page tests", function () {
 
         page._displays.length.should.eql(1);
     });
+
+    it('custom methods', function (done) {
+        var steps = 1;
+
+        JA.addPage({
+            id: "pageTest",
+            beforeDeactivate: function (next) {
+                steps.should.be.eql(3);
+                steps++;
+                next();
+            },
+            afterDeactivate: function () { 
+                steps.should.be.eql(4);
+                steps++;
+            },
+
+            beforeActivate: function (next) {
+                steps.should.be.eql(1);
+                steps++;
+                next();
+            },
+            afterActivate: function () {
+                steps.should.be.eql(2);
+                steps++;
+            }
+        });
+
+        JA.addPage({
+            id: 'JAPage',
+            beforeActivate: function (next) {
+                steps.should.be.eql(5);
+                steps++;
+                next();
+            },
+            afterActivate: function () {
+                steps.should.be.eql(6);
+                done();
+            }
+        });
+
+        JA.navigate('pageTest');
+        JA.navigate('JAPage');
+    });
 });
